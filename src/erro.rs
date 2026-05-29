88f9f8be05aa7edf133f37b710fc9bdb04fcccae
calebgriffin <caleb@azobenzene.tetrahedral.net>
2026-05-28 21:38:21 -0500

    Made errors generic with example

--- src/erro.rs
+++ src/erro.rs
@@ -4,6 +4,9 @@ use std::fmt;
 use std::error;
 use std::num::ParseIntError;
 
+// Make it visible in output what caller had what error
+// Currently it just says the error, but not the caller.
+
 pub type Result<T> = result::Result<T, Err>;
 
 // Used AI to figure out the dyn + Send + Sync
@@ -53,4 +56,10 @@ pub fn test_err(x: i32) -> Result<i32> {
     else {Err(Err::IoError(
         io::Error::new(io::ErrorKind::Other, "Caleb error message")))
     }
+}
+
+pub fn test_err2(x: i32) -> Result<u32> {
+    let val = TryInto::<u32>::try_into(x)
+        .map_err(|e| Err::Other(Box::new(e)))?;
+    Ok(val)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod cli;
 mod erro;
 // mod math;
-// fn main() -> anyhow::Result<()> {
 
 fn main() -> erro::Result<()> {
     let args: cli::Params = cli::get_args()?;
     println!("res = {:?}", args.res);
 
     // Testing errors
-    let test: i32 = erro::test_err(-2)?;
+    let _test: i32 = erro::test_err(1)?;
+
+    let _test2: u32 = erro::test_err2(1)?;
+
+    print!("yay!\n");
 
     Ok(())
 }
