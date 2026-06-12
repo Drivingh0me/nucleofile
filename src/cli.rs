44e601dd1af8968f336113d899b5bf3ed83d3380
Drivingh0me <drivingh0me@icloud.com>
2026-06-11 19:37:50 -0500

    working on structure

--- src/cli.rs
+++ src/cli.rs
@@ -35,6 +35,11 @@ pub fn get_args() -> Result<Params>
 {
     let args = Args::parse();
 
+    // let res: render::Resolution = args.resolution
+    //     .unwrap_or(String::from("640x480"))
+    //     .split('x') // Convert iter to Resolution
+
+
     let res = args.resolution.unwrap_or(String::from("640x480"))
         .split('x')
         .collect::<Vec<&str>>()
@@ -42,15 +47,21 @@ pub fn get_args() -> Result<Params>
         .map(|x| x.parse().unwrap_or(256))
         .collect::<Vec<u16>>();
 
-    let res: [u16; 2] = match res.len() {
-        0 => [640, 480],
-        1 => [res[0], 256],
-        _ => [res[0], res[1]],
+    let res: render::Resolution = match res.len() {
+        0 => a,
+        1 => b,
+        _ => c,
     };
+
+    // let res: [u16; 2] = match res.len() {
+    //     0 => [640, 480],
+    //     1 => [res[0], 256],
+    //     _ => [res[0], res[1]],
+    // };
 
     // if let Some(file) = args.file {println!("File: {:?}", file)}
 
-    if args.debug {println!("nucleofile is in debug mode!")}
+    // if args.debug {println!("nucleofile is in debug mode!")}
 
     Ok(Params {
         res,
