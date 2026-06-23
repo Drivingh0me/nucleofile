a9cc7c7de6041ce5682778b4a112d78c730260b8
Drivingh0me <drivingh0me@icloud.com>
2026-06-23 18:59:38 -0500

    Wrking on parser still

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -4,7 +4,9 @@ use std::io::{self, Write};
 pub fn run_interpreter() -> Result<()> {
     let mut stdout = io::stdout();
     let mut input = String::new();
+    let mut words: Vec<&str> = Vec::with_capacity(32);
     let mut input_bytes:usize = 0;
+
     loop {
         print_input_arrow(&mut stdout)?;
         input.clear();
@@ -17,6 +19,26 @@ pub fn run_interpreter() -> Result<()> {
         if input == "clear\n" {
             clear_screen(&mut stdout)?;
         }
+
+        if input == "help\n" {
+            print_help(&mut stdout)?;
+        }
+
+        words = input.split(' ').collect();
+
+        if let Some(word) = words.get(0) {
+            if *word == "echo" {
+                for x in 1..words.len() {
+                    println!("{}", words[x]);
+                }
+            }
+        }
+
+        // if word == "echo" {
+        //     for x in 1..words.len() {
+        //         println!(x);
+        //     }
+        // }
     }
 
     Ok(())
@@ -32,7 +54,15 @@ fn print_input_arrow(std_out: &mut io::Stdout) -> Result<()> {
 
 fn clear_screen(std_out: &mut io::Stdout) -> Result<()> {
     let mut stdout_lock = std_out.lock();
-    let bytes = b"\x1b[2J";
+    let bytes = b"\x1b[2J\x1b[H";
+    stdout_lock.write_all(bytes)?;
+    stdout_lock.flush()?;
+    Ok(())
+}
+
+fn print_help(std_out: &mut io::Stdout) -> Result<()> {
+    let mut stdout_lock = std_out.lock();
+    let bytes = b"Help not implemented\n";
     stdout_lock.write_all(bytes)?;
     stdout_lock.flush()?;
     Ok(())
