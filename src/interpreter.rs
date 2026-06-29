48ad08dd93d13dcb925284fad7f7cfc00f7d3d9f
Drivingh0me <drivingh0me@icloud.com>
2026-06-29 11:43:37 -0500

    working on interperter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -5,7 +5,7 @@ pub fn run_interpreter() -> Result<()> {
     let mut stdout = io::stdout();
     let mut input = String::new();
     let mut words: Vec<&str> = Vec::with_capacity(32);
-    let mut input_bytes:usize = 0;
+    let mut input_bytes: usize = 0;
 
     loop {
         print_input_arrow(&mut stdout)?;
@@ -26,19 +26,22 @@ pub fn run_interpreter() -> Result<()> {
 
         words = input.split(' ').collect();
 
-        if let Some(word) = words.get(0) {
-            if *word == "echo" {
-                for x in 1..words.len() {
-                    println!("{}", words[x]);
-                }
-            }
-        }
-
-        // if word == "echo" {
-        //     for x in 1..words.len() {
-        //         println!(x);
+        // if let Some(word) = words.get(0) {
+        //     if *word == "echo" {
+        //         for x in 1..words.len() {
+        //             println!("{}", words[x]);
+        //         }
         //     }
         // }
+        if let Some(word) = words.get(0) {
+            if *word == "echo" {
+                let response: Vec<&str> = words
+                    .into_iter()
+                    .skip(1)
+                    .collect();
+                print_response(&mut stdout, &response);
+            }
+        }
     }
 
     Ok(())
@@ -64,6 +67,31 @@ fn print_help(std_out: &mut io::Stdout) -> Result<()> {
     let mut stdout_lock = std_out.lock();
     let bytes = b"Help not implemented\n";
     stdout_lock.write_all(bytes)?;
+    stdout_lock.flush()?;
+    Ok(())
+}
+
+fn print_response(
+    std_out: &mut io::Stdout,
+    response: &Vec<&str>) -> Result<()> {
+    let mut stdout_lock = std_out.lock();
+    let response_arrow = b"\x1b[32m> \x1b[0m";
+    stdout_lock.write_all(response_arrow)?;
+
+    // Sinitize response as a single byte vec with spaces
+    let response: Vec<&str> = response
+        .iter()
+        // .skip(1)
+        .flat_map(|s| [" ", *s])
+        .collect();
+    // let response = response.remove(0);
+
+    let response_bytes:Vec<u8> = response
+        .iter()
+        .flat_map(|s| s.as_bytes().iter().copied())
+        .collect();
+
+    stdout_lock.write_all(&response_bytes)?;
     stdout_lock.flush()?;
     Ok(())
 }
