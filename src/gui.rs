1df503de8493ddd46f29ca21a3bb506f8180c9d9
Drivingh0me <drivingh0me@icloud.com>
2026-08-07 14:47:57 -0500

    Added simple logo and more gui topbar options

--- src/gui.rs
+++ src/gui.rs
@@ -1,12 +1,31 @@
 use eframe::egui;
+use std::sync::Arc;
 
 use crate::error::Result;
 
 pub fn run_gui() -> Result<()> {
+    let icon = image::load_from_memory(include_bytes!("../assets/icon.png"))
+        .expect("Failed to load icon path")
+        .into_rgba8();
+
+    let (width, height) = icon.dimensions();
+    let icon = egui::IconData {
+        rgba: icon.into_raw(),
+        width,
+        height,
+    };
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+        .with_inner_size([640.0, 480.0])
+            .with_icon(Arc::new(icon)),
+        ..Default::default()
+    };
+
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "eframe Test",
-        native_options,
+        options,
         Box::new(|cc| Ok(Box::new(AppState::new(cc))))
     );
     Ok(())
@@ -35,18 +54,41 @@ impl eframe::App for AppState {
     fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
         egui::Panel::top("top_panel").show_inside(ui, |ui| {
             egui::MenuBar::new().ui(ui, |ui| {
-                ui.menu_button("file", |ui| {
+                ui.menu_button("File", |ui| {
                     if ui.button("Quit").clicked() {
                         ui.send_viewport_cmd(egui::ViewportCommand::Close);
                     }
                 });
                 ui.add_space(16.0);
-                // egui::widgets::global_theme_preference_buttons(ui);
+                ui.menu_button("Edit", |ui| {
+                    if ui.button("Clear").clicked() {
+                        self.label = "".to_string();
+                    }
+                });
+                ui.add_space(16.0);
+                ui.menu_button("View", |ui| {
+                    if ui.button("Clear").clicked() {
+                        self.label = "".to_string();
+                    }
+                });
+                ui.add_space(16.0);
+                ui.menu_button("Tools", |ui| {
+                    if ui.button("Clear").clicked() {
+                        self.label = "".to_string();
+                    }
+                });
+                ui.add_space(16.0);
+                ui.menu_button("Help", |ui| {
+                    if ui.button("Clear").clicked() {
+                        self.label = "".to_string();
+                    }
+                });
+                ui.add_space(16.0);
             });
         });
 
         egui::CentralPanel::default().show_inside(ui, |ui| {
-            ui.heading("Hello World!");
+            ui.heading(&mut self.label);
 
             ui.horizontal(|ui| {
                 ui.label("write something: ");
